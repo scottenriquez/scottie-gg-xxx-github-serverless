2805a8e3910e0c98d4b510fdab5e4f1dacf2067f
Scottie Enriquez <scott.c.enriquez@gmail.com>
2019-01-05 11:35:20 -0600

    Removed redundant response information from handler

--- index.js
+++ index.js
@@ -2,9 +2,5 @@ const githubService = require('./services/Github');
 
 exports.handler = async (event) => {
     const publicRepos = await githubService.getPublicRepos();
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(publicRepos),
-    };
-    return response;
+    return JSON.stringify(publicRepos);
 };
